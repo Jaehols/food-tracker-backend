41355da5dcc43098cab2eec8cc3f28c0b26f62a0
Jaehols <111752611+Jaehols@users.noreply.github.com>
2023-12-17 11:13:08 +0800

    Feature/initial basic api (#1)
    * Basic API controller and service method to test Kotlin API functionality
    
    * Basic Get and Post API endpoints implemented

---
+++ src/main/kotlin/com/example/foodtrackerbackend/DTO/FoodEntry.kt
@@ -0,0 +1,13 @@
+package com.example.foodtrackerbackend.DTO
+
+import java.sql.Timestamp
+import java.util.*
+
+class FoodEntry(
+    val entryId: UUID,
+    val userId: UUID,
+    val entryTime: Timestamp,
+    val mealDescription: String,
+    val additionalComments: String,
+    val kilojoules: Int,
+    )
---
+++ src/main/kotlin/com/example/foodtrackerbackend/controllers/FoodDiaryController.kt
@@ -0,0 +1,48 @@
+package com.example.foodtrackerbackend.controllers
+
+import com.example.foodtrackerbackend.DTO.FoodEntry
+import com.example.foodtrackerbackend.DTO.FoodEntryRequestBody
+import com.example.foodtrackerbackend.services.FoodEntryService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import java.net.URI
+import java.sql.Timestamp
+import java.util.*
+
+
+@RestController
+@RequestMapping("api/v1/food-diary")
+class FoodDiaryController(private val foodEntryService: FoodEntryService) {
+
+    @GetMapping("/entry/{entryId}")
+    fun getFoodEntryById(@PathVariable entryId: UUID): ResponseEntity<FoodEntry>{
+        //TODO actually implement this
+        val foodEntry = foodEntryService.GetFoodEntryById(entryId)
+        return if (foodEntry != null){
+            ResponseEntity.ok(foodEntry)
+        } else {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @PostMapping("/new-entry")
+    fun saveNewFoodEntry(@RequestBody foodEntryRequest: FoodEntryRequestBody): ResponseEntity<Void> {
+
+        val newEntryId = foodEntryService.SaveNewFoodEntry(
+                UUID.randomUUID(),
+                UUID.randomUUID(),
+                foodEntryRequest.entryTime,
+                foodEntryRequest.mealDescription,
+                foodEntryRequest.additionalComments,
+                foodEntryRequest.kilojoules
+        )
+
+        val location: URI = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/entry/{newEntryId}")
+            .buildAndExpand(newEntryId)
+            .toUri()
+
+        return ResponseEntity.created(location).build()
+    }
+}
