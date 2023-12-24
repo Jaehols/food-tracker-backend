1cad2cb1bddfa97a1e47bd771e7fe7678669e8c1
Jaehols <jameswh@iinet.net.au>
2023-12-24 16:04:47 +0800

    Feature/connection through to database (#2)
    * Testing to see if this adds the gitignore
    
    * stop tracking app properties
    
    * Got the mongo client DI working finally
    
    * Switched over to webflux to take advantage of coroutines
    
    * Saving and retrieving data from database in most basic form finished
    
    * Cleaned up some magic strings

--- src/main/kotlin/com/example/foodtrackerbackend/services/FoodEntryService.kt
+++ src/main/kotlin/com/example/foodtrackerbackend/services/FoodEntryService.kt
@@ -1,22 +1,63 @@
 package com.example.foodtrackerbackend.services
 
 import com.example.foodtrackerbackend.DTO.FoodEntry
+import com.example.foodtrackerbackend.Utilities.MongoUtilities.Companion.getOrCreateCollection
+import com.mongodb.client.model.Filters.eq
+import com.mongodb.client.model.InsertOneOptions
+import com.mongodb.kotlin.client.coroutine.MongoClient
+import kotlinx.coroutines.flow.firstOrNull
+import org.bson.Document
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.sql.Timestamp
 import java.util.*
 
 @Service
-class FoodEntryService {
+class FoodEntryService(@Autowired private val mongoClient: MongoClient) {
 
-    fun getFoodEntryById(entryId: UUID): FoodEntry? {
-        //TODO implement proper function
-        val foodEntry = FoodEntry(entryId, UUID.randomUUID(), Timestamp(System.currentTimeMillis()), "Handful of nuts", "It was yummy", 200)
-        return foodEntry
+    companion object {
+        private const val DATABASE_NAME = "food-diary"
+        private const val ENTRY_ID_FIELD = "entryId"
+        private const val USER_ID_FIELD = "userId"
+        private const val ENTRY_TIME_FIELD = "entryTime"
+        private const val CREATED_TIME_FIELD = "createdTime"
+        private const val MEAL_DESCRIPTION_FIELD = "mealDescription"
+        private const val ADDITIONAL_COMMENTS_FIELD = "additionalComments"
+        private const val KILOJOULES_FIELD = "kilojoules"
+        private const val ENTRY_TYPE_FIELD = "entryType"
     }
-    fun saveNewFoodEntry(entryId: UUID, userId: UUID, entryTime: Timestamp, mealDescription: String, additionalComments: String, kilojoules: Int): UUID {
-        //TODO save down to database
-        val foodEntry = FoodEntry(entryId, userId, entryTime, mealDescription, additionalComments, kilojoules)
-        println(foodEntry)
+
+    val database = mongoClient.getDatabase(DATABASE_NAME)
+    suspend fun getFoodEntryById(entryId: UUID, userId: String): FoodEntry? {
+        val collection = getOrCreateCollection(database, userId)
+        val document = collection.find(eq(ENTRY_ID_FIELD, entryId.toString())).firstOrNull()
+
+        return document?.let { doc ->
+            FoodEntry(
+                entryId = UUID.fromString(doc.getString(ENTRY_ID_FIELD)),
+                userId = doc.getString(USER_ID_FIELD),
+                entryTime = doc.getDate(ENTRY_TIME_FIELD),
+                createdTime = doc.getDate(CREATED_TIME_FIELD),
+                mealDescription = doc.getString(MEAL_DESCRIPTION_FIELD),
+                additionalComments = doc.getString(ADDITIONAL_COMMENTS_FIELD),
+                kilojoules = doc.getInteger(KILOJOULES_FIELD)
+            )
+        }
+    }
+    suspend fun saveNewFoodEntry(entryId: UUID, userId: String, entryTime: Date, mealDescription: String, additionalComments: String, kilojoules: Int): UUID {
+
+        val foodEntry = FoodEntry(entryId, userId, entryTime, Date(), mealDescription, additionalComments, kilojoules)
+        val collection = getOrCreateCollection(database, userId)
+        val document = Document()
+            .append(ENTRY_ID_FIELD, foodEntry.entryId.toString())
+            .append(USER_ID_FIELD, foodEntry.userId)
+            .append(ENTRY_TIME_FIELD, foodEntry.entryTime)
+            .append(CREATED_TIME_FIELD, foodEntry.createdTime)
+            .append(MEAL_DESCRIPTION_FIELD, foodEntry.mealDescription)
+            .append(ADDITIONAL_COMMENTS_FIELD, foodEntry.additionalComments)
+            .append(KILOJOULES_FIELD, foodEntry.kilojoules)
+            .append(ENTRY_TYPE_FIELD, foodEntry.entryType)
+
+        collection.insertOne(document, InsertOneOptions().bypassDocumentValidation(false))
         return foodEntry.entryId
     }
 }
