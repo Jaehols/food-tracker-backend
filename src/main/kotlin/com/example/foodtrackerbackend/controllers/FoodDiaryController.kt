af032ab67225e9baaacfa3b1e9221d82e48fe60a
Jaehols <jameswh@iinet.net.au>
2023-12-29 14:34:39 +0800

    Feature/get all food entries for user (#3)
    * Added functionality to get list of entries
    
    * Updated api to allow for getting a single day

--- src/main/kotlin/com/example/foodtrackerbackend/controllers/FoodDiaryController.kt
+++ src/main/kotlin/com/example/foodtrackerbackend/controllers/FoodDiaryController.kt
@@ -3,11 +3,13 @@ package com.example.foodtrackerbackend.controllers
 import com.example.foodtrackerbackend.DTO.FoodEntry
 import com.example.foodtrackerbackend.DTO.FoodEntryRequestBody
 import com.example.foodtrackerbackend.services.FoodEntryService
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
+import java.time.LocalDate
 import java.util.*
 
 private const val baseAddress: String = "api/v1/food-diary"
@@ -23,6 +25,21 @@ class FoodDiaryController(private val foodEntryService: FoodEntryService) {
         } else {
             ResponseEntity.notFound().build()
         }
+    }
+
+    @GetMapping("entriesByAuthor/{authorId}")
+    suspend fun getAllFoodEntriesByAuthorId(
+        @PathVariable authorId: String,
+        @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) startDate: LocalDate?,
+        @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) endDate: LocalDate?
+    ): ResponseEntity<List<FoodEntry>> {
+        val entries = if (startDate != null && endDate != null) {
+            foodEntryService.getFoodEntriesByAuthorIdAndDateRange(authorId, startDate, endDate)
+        } else {
+            foodEntryService.getFoodEntriesByAuthorId(authorId)
+        }
+
+        return ResponseEntity.ok(entries)
     }
 
     @PostMapping("/new-entry")
